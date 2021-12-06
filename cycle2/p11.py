5e7cb9645828471ed23acdc663db118200f1a529
Harithahari16 <74651763+Harithahari16@users.noreply.github.com>
2021-12-06 11:15:17 +0530

    Add files via upload

---
+++ cycle2/p11.py
@@ -0,0 +1,7 @@
+import numpy as np 
+a = [20, 2, .2, 10, 4]  
+b = ([[3, 6,8,7], [4, 2,1,0],[3,1,3,3],[1,1,2,2]]) 
+print("1D array \n",a)  
+print("2D array \n",b)  
+print("\nSum of 1D array : ", np.sum(a)) 
+print("\nSum of 2D array : ", np.sum(b)) 
